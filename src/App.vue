08788b35eadb53cc9a9d3c1cf3686dbccdbe0dfe
matteopoliti <matteo.politi17@gmail.com>
2024-02-09 14:35:29 +0100

    Feat: add axios and store.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script>
+import axios from "axios";
+import { store } from "./store";
 
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+            store,
+        };
+    },
+    created() {
+    },
+}
 </script>
 
 <template></template>
